f857a3fbd1922304849e6177a405242bedb139e5
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-03-27 12:58:04 +0100

    added design carousel with viewPager

--- app/src/main/java/com/thedeveloperworldisyours/lope/animationtransition/viewpager/ViewPagerTransitionActivity.java
+++ app/src/main/java/com/thedeveloperworldisyours/lope/animationtransition/viewpager/ViewPagerTransitionActivity.java
@@ -3,6 +3,9 @@ package com.thedeveloperworldisyours.lope.animationtransition.viewpager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.thedeveloperworldisyours.lope.R;
 
@@ -10,6 +13,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ViewPagerTransitionActivity extends AppCompatActivity {
+
+    @BindView(R.id.view_pager_transition_activity_toolbar)
+    Toolbar mToolbar;
 
     @BindView(R.id.view_pager_transition_activity_view_pager)
     ViewPager mViewPager;
@@ -40,6 +46,34 @@ public class ViewPagerTransitionActivity extends AppCompatActivity {
 
         // Set margin for pages as a negative number, so a part of next and
         // previous pages will be showed
-        mViewPager.setPageMargin(-400);
+        mViewPager.setPageMargin(-200);
+
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void back(View view) {
+        finishMyActivity();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishMyActivity();
+    }
+
+    public void finishMyActivity() {
+        finish();
+        overridePendingTransition(R.anim.gmail_back_in, R.anim.gmail_back_out);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finishMyActivity();
+                break;
+        }
+        return true;
     }
 }
