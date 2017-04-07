ef48069cac0acb6eb202778b3e80bdbed5c3be7b
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-04-07 10:33:15 +0100

    added activity for animation recyclerview

--- app/src/main/java/com/thedeveloperworldisyours/lope/animationtransition/recyclerview/InitAnimationRecyclerViewActivity.java
+++ app/src/main/java/com/thedeveloperworldisyours/lope/animationtransition/recyclerview/InitAnimationRecyclerViewActivity.java
@@ -1,6 +1,5 @@
-package com.thedeveloperworldisyours.lope.animationtransition;
+package com.thedeveloperworldisyours.lope.animationtransition.recyclerview;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,21 +7,21 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.thedeveloperworldisyours.lope.R;
-import com.thedeveloperworldisyours.lope.animationtransition.recyclerview.InitAnimationRecyclerViewActivity;
-import com.thedeveloperworldisyours.lope.animationtransition.viewpager.ViewPagerTransitionActivity;
-import com.thedeveloperworldisyours.lope.transition.ElementStartActivity;
-import com.thedeveloperworldisyours.lope.transition.VerticalRecyclerViewActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MenuAnimationTransitionActivity extends AppCompatActivity {
-    @BindView(R.id.menu_animation_transition_activity_toolbar)
+public class InitAnimationRecyclerViewActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.init_animation_recycler_view_activity_toolbar)
     Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_animation_transition_activity);
+        setContentView(R.layout.init_animation_recycler_view_activity);
+        overridePendingTransition(R.anim.appearance_go_in, R.anim.appearance_go_out);
 
         ButterKnife.bind(this);
 
@@ -42,6 +41,7 @@ public class MenuAnimationTransitionActivity extends AppCompatActivity {
 
     public void finishMyActivity() {
         finish();
+        overridePendingTransition(R.anim.appearance_back_in, R.anim.appearance_back_out);
     }
 
     @Override
@@ -53,15 +53,4 @@ public class MenuAnimationTransitionActivity extends AppCompatActivity {
         }
         return true;
     }
-
-    public void goToViewPagerTransition(View view) {
-        Intent intent = new Intent(this, ViewPagerTransitionActivity.class);
-        startActivity(intent);
-    }
-
-    public void gotToInitAnimationRecyclerView(View view) {
-        Intent intent = new Intent(this, InitAnimationRecyclerViewActivity.class);
-        startActivity(intent);
-    }
 }
-
