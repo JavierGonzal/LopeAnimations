9da5750512f5d1168c0df5c95dd1f5dd3295e01f
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-05-30 12:09:07 +0100

    added image in collapsing

--- app/src/main/java/com/thedeveloperworldisyours/lope/transition/CollapsingToolbarActivity.java
+++ app/src/main/java/com/thedeveloperworldisyours/lope/transition/CollapsingToolbarActivity.java
@@ -1,25 +1,35 @@
 package com.thedeveloperworldisyours.lope.transition;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.thedeveloperworldisyours.lope.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CollapsingToolbarActivity extends AppCompatActivity {
+
+    @BindView(R.id.collapsing_toolbar_title)
+    ImageView mImageView;
+
+    @BindView(R.id.collapsing_toolbar_toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collapsing_toolbar_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setOnClickListener(new View.OnClickListener() {
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        mToolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCollapsing();
@@ -29,8 +39,10 @@ public class CollapsingToolbarActivity extends AppCompatActivity {
     }
 
     public void goToCollapsing() {
-                Intent intent = new Intent(this, CollapsingActivity.class);
-                startActivity(intent);
+        Intent intent = new Intent(this, CollapsingActivity.class);
+        String transitionName = getString(R.string.blue_name);
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(this, mImageView, transitionName);
+        startActivity(intent);
     }
 
 }
